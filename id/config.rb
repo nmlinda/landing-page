9c7dadbb6113b8abef3f175684b8075d5712b8c0
Nur Meilinda Nugraha <nmlinda97@gmail.com>
2019-05-14 13:04:33 +0700

    tes

--- id/config.rb
+++ id/config.rb
@@ -13,7 +13,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-set :data_dir, 'source/data'
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
